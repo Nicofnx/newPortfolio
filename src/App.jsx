94c4fcedd41dbe2564f447a8e15d7ccd8b684c20
Nicolas Rodriguez <nicolasarielrodriguez@gmail.com>
2022-12-20 14:21:42 -0300

    primer commit porfolio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import NavBar from './components/Navbar/NavBar'
+import PhotoAndData from './components/PhotoAndData/PhotoAndData'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <NavBar/>
+      <PhotoAndData/>
+    </div>
+  )
+}
+
+export default App
